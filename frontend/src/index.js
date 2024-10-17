d0fbce9717d3e20906127c6f1321fbdb4a66b13b
Vithushayini <vithushayini2000@gmail.com>
2024-10-17 23:41:47 +0530

    tailwindcss installed and index.js created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { AuthProvider } from "@asgardeo/auth-react";
+import './index.css'; 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const authConfig = {
+
+  clientID:"NVIfXFkgYD5yR4CQ6sWHYeoVkXka",
+  baseUrl:"https://api.asgardeo.io/t/vithushayini",
+  signInRedirectURL:"http://localhost:3000/",
+  signOutRedirectURL:"http://localhost:3000/",
+  scope: [ "openid", "profile","email","address","phone"]
+
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider config={authConfig}>
+      <App />
+    </AuthProvider>,
   </React.StrictMode>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
